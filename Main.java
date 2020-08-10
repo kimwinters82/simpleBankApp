17e7484c6045e094b05f6e17736d544cd413a7b6
Kim <kim.winters82@gmail.com>
2020-08-10 11:37:30 +0000

    Created class and 3 objects

--- Main.java
+++ Main.java
@@ -1,3 +1,21 @@
+class Bank{
+  String user;
+  String accType;
+  double startBalance;
+  double currentBalance;
+}
+
+Bank(String u, String a, double s){
+  user = u;
+  accType = a;
+  startBalance = s;
+
+}
+
+Bank user1 = new Bank("Kim", "Deposit", 500);
+Bank user2 = new Bank("Paddy", "Current", 2000);
+Bank user3 = new Bank("Eileen", "Savings", 4000);
+
 class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
