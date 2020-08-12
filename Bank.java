a6cf480d148e8f3ca717d79e9ac0cb851d820244
Kim <kim.winters82@gmail.com>
2020-08-12 10:24:00 +0000

    Successfully ask user if they have an account and ask for acc num if they do, ask to create acc if don't

--- Bank.java
+++ Bank.java
@@ -1,22 +1,20 @@
-import java.util.Scanner;
+import java.util.*;
 
-
+// create public class 
 public class Bank{
   String user;
   String accType;
   double balance;
+  int accNum;
   
-
-
-Bank(String u, String a, double s){
+// create constructor for creating new users
+Bank(String u, String a, double s, int ac){
   user = u;
   accType = a;
   balance = s;
-
+  accNum = ac;
 }
-
-
-
+// methods
 void withdraw(){
  Scanner input = new Scanner(System.in);
  System.out.println(user + ", how much would you like to withdraw?");
@@ -36,4 +34,10 @@ void deposit(){
 void checkBalance(){
   System.out.println(user + ", your balance is " + balance);
 }
+
+public String toString(){
+  return (user + " " + accType + " " + balance + " " + accNum);
+}
+
+
 }
--- Main.java
+++ Main.java
@@ -1,14 +1,45 @@
-
+import java.util.*;
 
 class Main {
   public static void main(String[] args) {
-    
-    Bank user1 = new Bank("Kim", "Deposit", 500);
-    Bank user2 = new Bank("Paddy", "Current", 2000);
-    Bank user3 = new Bank("Eileen", "Savings", 4000);
-    
-    user1.withdraw();
-    user2.deposit();
-    user3.checkBalance();
+    //create arraylist to store all users information 
+    ArrayList<Bank> users = new ArrayList();
+    //create some users
+    Bank user1 = new Bank("Kim", "Deposit Account", 500, 2345);
+    Bank user2 = new Bank("Paddy", "Current Account", 2000, 1234);
+    Bank user3 = new Bank("Eileen", "Savings Account", 4000, 1456);
+    //add users to arraylist
+    users.add(user1);
+    users.add(user2);
+    users.add(user3);
+
+    //to check what in stored in users
+  System.out.println(users.toString());
+
+  //either access their account or ask them to make a new one
+  Scanner input1 = new Scanner(System.in);
+  System.out.println("Hello! Do you already have an account? Y or N");
+  String accHolder = input1.next();
+  
+  if(accHolder.equalsIgnoreCase("Y")){
+    Scanner input2 = new Scanner(System.in); 
+    System.out.println("Please enter your Account Number: ");
+   int num = input2.nextInt(); 
+
+  if(num == user1.accNum){
+    System.out.println("yay");
+  }else{
+    System.out.println("Please enter a valid account number: ");
+  
   }
+  }else{
+    System.out.println("Please create an account");
+  }
+  
+}
+   
+  
+  
+  
+  
 }
